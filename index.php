067407ffbc35d043d93d3a98905e71a088516339
Peter Street <PeterBStreet@gmail.com>
2018-01-09 11:54:20 -0700

    Peter Street added hobby message

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 		<h1>This is a page about Tristan Bennett</h1>
 		<p>I grew up with 2 dogs and a cat</p>
 		<p>Now I live with 4 cats....</p>
+		<div>
 		<img src="dog-cat-friends.jpg" alt="Dog and Cat Friends">
+		</div>
+			<p>My new hobby is <em>refinishing</em> furniture</p>
 	</body>
 </html>
