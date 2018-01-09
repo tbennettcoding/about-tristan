fd1256988d649fdc8a43e38633ae394852752a78
Tristan Bennett <tmbennett2011@msn.com>
2018-01-09 09:49:46 -0700

    created index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>About Tristan</title>
+	</head>
+	<body>
+		<h1>This is a page about Tristan Bennett</h1>
+	</body>
+</html>
